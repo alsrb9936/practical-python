ccf0958b313a04f72259dfd6fffe1307f05feb5a
PulledGyu <ghfn123@gmail.com>
2021-03-20 13:48:13 +0000

    Solved 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,15 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
 
 def portfolio_cost(filename):
     total_cost = 0
     try:
-        with open(filename, 'rt') as f:
-            hearder = next(f)
+        with open(filename) as f:
+            rows = csv.reader(f)
+            hearder = next(rows)
 
-            for line in f:
-                row = line.split(',')
+            for row in rows:
                 amount = row[1]
                 cost = row[2]
                 total_cost += int(amount) * float(cost)
