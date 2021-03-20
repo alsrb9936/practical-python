9bbd28fadf5436b1c64e3b20cf1610c461722ffb
PulledGyu <ghfn123@gmail.com>
2021-03-20 13:44:08 +0000

    Solved 1.31

--- Work/pcost.py
+++ Work/pcost.py
@@ -4,18 +4,20 @@
 
 def portfolio_cost(filename):
     total_cost = 0
+    try:
+        with open(filename, 'rt') as f:
+            hearder = next(f)
 
-    with open(filename, 'rt') as f:
-        hearder = next(f)
-
-        for line in f:
-            row = line.split(',')
-            amount = row[1]
-            cost = row[2]
-            total_cost += int(amount) * float(cost)
+            for line in f:
+                row = line.split(',')
+                amount = row[1]
+                cost = row[2]
+                total_cost += int(amount) * float(cost)
+            
+            return total_cost
         
-    return total_cost
-
+    except ValueError:
+        print("Error!")
     
 cost = portfolio_cost('Data/portfolio.csv')
 print('Total Cost:', cost)
