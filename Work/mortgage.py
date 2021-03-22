0e28de3d139167147e1c5fc4fd005a64dd049055
PulledGyu <ghfn123@gmail.com>
2021-03-22 02:39:12 +0000

    Modified mortage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -9,16 +9,16 @@ principal = 500000.0
 rate = 0.05 
 payment = 2684.11
 total_paid = 0.0
-month = 0;
+month = 0
 
 while principal > 0:
-    month += 1;
+    month += 1
     if principal < payment:
         total_paid += principal
         principal = 0
         
     else:
-        principal = principal * (1+rate/12) - payment 
+        principal = principal * (1+rate/12) - payment
         total_paid = total_paid + payment
     
     if month >= extra_payment_start_month and month <= extra_payment_end_month:
