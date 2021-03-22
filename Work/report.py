5bb96054b40f94839427e0073f58d6f16e5f9c42
PulledGyu <ghfn123@gmail.com>
2021-03-22 03:42:51 +0000

    Solved 2.7

--- Work/report.py
+++ Work/report.py
@@ -17,3 +17,35 @@ def read_portfolio(filename):
             
     return portfolio
 
+def read_prices(filename):
+    prices = {}
+    
+    with open(filename, 'r') as f:
+        rows = csv.reader(f)
+        
+        try:
+            for row in rows:
+                prices[row[0]] = float(row[1])
+        
+        except IndexError:
+            print('더 이상 참조할 값이 없습니다.')
+            
+    return prices
+
+portfolio_file = 'Data/portfolio.csv'
+prices_file = 'Data/prices.csv'
+
+portfolio = read_portfolio(portfolio_file)
+prices = read_prices(prices_file)
+
+Gain = 0
+
+for i in portfolio:
+    portfolio_cost = i['price'] 
+    now_cost = prices[i['name']]
+    shares = i['shares']
+    
+    Gain += shares * (now_cost - portfolio_cost)
+    
+print('Gain: ', Gain)
+    
